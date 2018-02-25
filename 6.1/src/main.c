440de8912ef88f64b6e969a906c88c1908482cb8
Marcos Collado <marquitos.mcm@gmail.com>
2018-02-25 16:51:14 +0100

    Ejercicio 6.1. alarma basica:
     * Arreglado tiempo de rebote
     * Arregladas transiciones

--- 6.1/src/main.c
+++ 6.1/src/main.c
@@ -7,7 +7,7 @@
 #define PIN_D8 15
 #define LED 2
 #define PERIOD_TICK 100/portTICK_RATE_MS
-#define REBOUND_TIME 400
+#define REBOUND_TIME 400/portTICK_RATE_MS
 #define ETS_GPIO_INTR_DISABLE() \
   _xt_isr_mask(1 << ETS_GPIO_INUM)
 #define ETS_GPIO_INTR_ENABLE() \
@@ -20,13 +20,15 @@ enum alarm_state {
   ALARM_OFF
 };
 
-long reboundTimeout;
+portTickType reboundTimeout;
 volatile bool pressed = false;
+volatile bool isPresent = false;
 int checkIfPressed(fsm_t*);
-void led_on();
+int isSomeoneHere(fsm_t*);
+void led_on(fsm_t*);
 void led_off(fsm_t*);
+void clearFlags(fsm_t*);
 void isr_gpio();
-long getCurrentTime();
 
 /******************************************************************************
  * FunctionName : user_rf_cal_sector_set
@@ -72,8 +74,9 @@ uint32 user_rf_cal_sector_set(void)
 }
 
 struct fsm_trans_t alarm[] = {
+  {ALARM_OFF, checkIfPressed, ALARM_ON, clearFlags},
   {ALARM_ON, checkIfPressed, ALARM_OFF, led_off},
-  {ALARM_OFF, checkIfPressed, ALARM_ON, led_off},
+  {ALARM_ON, isSomeoneHere, ALARM_ON, led_on},
   {-1, NULL, -1, NULL},
 };
 
@@ -100,35 +103,38 @@ int checkIfPressed(fsm_t* this) {
   return pressed;
 }
 
-void led_on () {
+int isSomeoneHere(fsm_t* this) {
+  return isPresent;
+}
+
+void led_on (fsm_t* this) {
   GPIO_OUTPUT_SET(LED, 0);
+  isPresent = false;
 }
 
 void led_off (fsm_t* this) {
   GPIO_OUTPUT_SET(LED, 1);
-  pressed = false;
+  clearFlags(this);
 }
 
-long getCurrentTime() {
-  return xTaskGetTickCount()*portTICK_RATE_MS;
+void clearFlags(fsm_t* this) {
+  isPresent = false;
+  pressed = false;
 }
 
 void isr_gpio() {
   uint32 status = GPIO_REG_READ(GPIO_STATUS_ADDRESS);
   static bool isActive = false;
-  long now = getCurrentTime();
+  long now = xTaskGetTickCount();
   // Atencion a la interrupcion del boton de armar/desarmar
   if (status & BIT(BUTTON_D3)) {
     if (now > reboundTimeout) {
       reboundTimeout = now + REBOUND_TIME;
       pressed = true;
-      isActive = !isActive;
     }
     // Atencion a la interrupcion del pin de presencia del intruso
   } else if (status & BIT(PIN_D8)) {
-    if (isActive) {
-      led_on();
-    }
+    isPresent = true;
   }
 
   GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, status);
